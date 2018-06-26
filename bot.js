935be0d2ffbf1d4d3d8788c3105ef428b7339505
7sena <40254036+7sena@users.noreply.github.com>
2018-06-26 23:23:50 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,175 +1,53 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
-client.on('ready', () => {FireBot | $help  .`,'https://www.twitch.tv/v5bz');
-  console.log('---------------');
-  console.log(' Bot Is Online')
-  console.log('---------------')
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+    if (message.content === 'hla') {
+    	message.channel.send(':heart: هلا بيك منور سيرفرنا :heart:');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'Back') {
+    	message.reply(':heart: مرحبا واهلن فيك :heart:');
+  	}
 });
 client.on('message', message => {
-     if (message.content === "-servers") {
-     let embed = new Discord.RichEmbed()
-  .setColor("#0000FF")
-  .addField("**Server: **" , client.guilds.size)
-  message.channel.sendEmbed(embed);
-    }
-
-client.on('message', msg => {
-  if (msg.content === '$ping') {
-    msg.reply('Pong!');
-  }
+    if (message.content === '$server') {
+    	message.reply('"SERVER BY" ,":heart: server by KillerFox :heart:"');
+  	}
 });
+client.on('message' , message => {
 if (message.content === '$help') {
               var embed  = new Discord.RichEmbed()
-                .addField("**LINKS**" ,":no_entry_sign: تم اضافه منع الروابط :no_entry_sign: ")
-				.addField("**Bot**" ," :heart: Bot By KillerFox_:heart: ")
-                .addField("broadcast (bc)" ,"**الاستخدام:** ``$broadcast <الرساله> , $bc <الرساله>``")
-                .addField("**BAN**" ,"**الاستخدام:** ``$ban <المستخدم>``")
-                .addField("**KICK**" ,"**الاستخدام:** ``$kick <المستخدم> ``")
-                .addField("**ِAVATAR**" ,"**الاستخدام:** ``$avatar``")
-                .addField("**INFO**", "**الأستخدام :** ``$info``")
-                .addField("**SAY**" ,"**الاستخدام:** ``$say <الرساله>``")
-                .addField("**ID**" ,"**ال��ستخدام:** ``$id``")
-                .addField("**SERVER**" ,"**الاستخدام:** ``$server``")
-                .addField("**INVITE**" ,"**الاستخدام:** ``$invite <لأضافه البوت لأى سيرفر>``")
-                .addField("**SUPPORT**" ,"**الاستخدام:** ``$suppport <سيرفر دعم الفنى>``")
-                .addField("**QA**" ,"**الاستخدام:** ``$qa <السؤال>``  ")
-                .addField("**CLEAR**" ,"**الاستخدام:** ``$clear <العدد>``")
-                .addField("**PING**", "**الأستخدام:** ``$ping``")
-                .addField("**SERVERNAME**", "**الأستخدام:** ``$servername``")
-                .addField("**CUT_TWEET**", "**الاستخدام** ``$ct``")
-                .addField("**TWEET**", "**الاستخدام** ``$tweet <الرساله>``")
-                .addField("**NO INVITE LINKE**","تم اضافة خاصية منع الانفيتات ")
-                .addField("**LOGIN**" , " تم اضافة خاصية التفعيل لطلب تشغيلها في السيرفر كلم المبيرمجين ")
+                .addField("LINKS" ,":no_entry_sign: تم اضافه منع الروابط :no_entry_sign: ")
+                .addField("broadcast (bc)" ,"الاستخدام: $broadcast <الرساله> , $bc <الرساله>")
+                .addField("BAN" ,"الاستخدام: $ban <المستخدم>")
+                .addField("KICK" ,"الاستخدام: $kick <المستخدم>")
+                .addField("ِAVATAR" ,"الاستخدام: $avatar")
+                .addField("INFO", "الأستخدام : $info")
+                .addField("SAY" ,"الاستخدام: $say <الرساله>")
+                .addField("ID" ,"ال��ستخدام: $id")
+                .addField("SERVER" ,"الاستخدام: $server")
+                .addField("INVITE" ,"الاستخدام: $invite <لأضافه البوت لأى سيرفر>")
+                .addField("SUPPORT" ,"الاستخدام: $suppport <سيرفر دعم الفنى>")
+                .addField("QA" ,"الاستخدام: $qa <السؤال>  ")
+                .addField("CLEAR" ,"الاستخدام: $clear <العدد>")
+                .addField("PING", "الأستخدام: $ping")
+                .addField("SERVERNAME", "الأستخدام: $servername")
+                .addField("CUT_TWEET", "الاستخدام $ct")
+                .addField("TWEET", "الاستخدام $tweet <الرساله>")
+                .addField("NO INVITE LINKE","تم اضافة خاصية منع الانفيتات ")
+                .addField("LOGIN" , " تم اضافة خاصية التفعيل لطلب تشغيلها في السيرفر كلم المبيرمجين ")
                 .setColor('RANDOM')
 .setColor('RANDOM')
   message.author.sendEmbed(embed);
     }
 });
-client.on('message', message => {
-  if(message.content === ('clear')) {
-  let modRole = message.guild.roles.find("name", "Admin");
-  if (!modRole) return message.reply('You do not have Admin Role'); {
-    }
-  const params = message.content.split(" ").slice(1)
-    let messagecount = parseInt(params[0]);
-    message.channel.fetchMessages({limit: messagecount})
-        .then(messages => message.channel.bulkDelete(messages));
-  }
-});
-client.on('message', message => {
-    var args = message.content.split(/[ ]+/)
-    if(message.content.includes('discord.gg')){
-      if(!message.member.hasPermission('ADMINISTRATOR'))
-        message.delete()
-    return message.reply(`** No Invite Links :angry: ! **`)
-    }
-});
-client.on('message', msg => {
-  if (msg.content === '$invite') {
-    msg.reply('link');
-  }
-});
-client.on('message', msg => {
-  if (msg.content === '$help') {
-    msg.reply('*FirelBot* | شوف الخاص');
-  }
-});
-client.on("message", message => {
-    var prefix = "-";
- 
-            var args = message.content.substring(prefix.length).split(" ");
-            if (message.content.startsWith(prefix + "clear")) {
-   if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('⚠ | ** ما عندك صلاحيه **');
-        var msg;
-        msg = parseInt();
-      
-      message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
-      message.channel.sendMessage("", {embed: {
-        title: "Done | تــم",
-        color: 0x06DF00,
-        description: "تم ازالة جميع رساىل",
-        footer: {
-          text: "ٍSroryCRaft."
-        }
-      }}).then(msg => {msg.delete(3000)});
-                          }
 
-     
-});
-// Your Avatar URL!
-client.on('message', message => {
-    if (message.content === "$Avatar") {
-    message.reply(message.author.avatarURL); 
-    }
-});
-client.on('message', msg => {
-  if (msg.content === '$suppport') {
-    msg.reply('السيرفر للمساعده,https://discord.gg/a5T4zQ');
-  }
-});
-client.on("message", message => {
-      if (message.content === "$ping") {
-      const embed = new Discord.RichEmbed()
-  .setColor("RANDOM")
-  .addField('**Ping:**' , `${Date.now() - message.createdTimestamp}` + ' ms')
-  message.channel.sendEmbed(embed);
-    }
-});
-    client.on('message', message => {
-     if (message.content === "$id") {
-     let embed = new Discord.RichEmbed()
-  .setThumbnail(message.author.avatarURL)  
-  .setAuthor(message.author.username)
-.setDescription("معلومات عن الحــساب")
-               .setFooter(`Name Bot.`, '')
-  .setColor("#9B59B6")
-  .addField("اســـم الحســاب", `${message.author.username}`)
-  .addField('كود الحساب الخاص', message.author.discriminator)
-  .addField("الرقـــم الشـــخصي", message.author.id)
-  .addField('بــــوت', message.author.bot)
-  .addField("تاريخ التسجيل", message.author.createdAt)
-     
-     
-  message.channel.sendEmbed(embed);
-    }
-});
-var prefix = "-";
-client.on('message', message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-
-  if (command == "say") {
-   message.channel.sendMessage(args.join("  "))
-  }
-});
-client.on("guildMemberRemove", member => {
-  let guild = member.guild;
-  guild.defaultChannel.sendMessage("", {embed: {
-  color: 808080,
-  author: {
-    name: member.user.username,
-    icon_url: member.user.avatarURL
-  },
-  title: guild.name,
-  description: ' Bye ..',
-}}).catch(console.error);
-  }
-);
-client.on("message", (message) => {
-    if (message.content.startsWith("*ban ")) {
-      if(!message.member.hasPermission('BAN_MEMBERS')) return message.reply('⚠ ماعندك الصلاحيات');
-        var member= message.mentions.members.first();
-        member.ban().then((member) => {
-            message.channel.send(member.displayName + " لقد تم طرده شخض:wave: ");
-        }).catch(() => {
-            message.channel.send(":x: هناك خطاء حاول مره أخرى:x: ");
-        });
-    }
-});
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
