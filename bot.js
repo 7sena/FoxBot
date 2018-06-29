f931294fde1bef1e0323d42ddd75e84d2da2ad7a
7sena <40254036+7sena@users.noreply.github.com>
2018-06-29 16:10:39 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,6 +14,19 @@ client.on('ready', () => {
 client.user.setGame(` Bot By KillerFox | $help `,"http://twitch.tv/S-F")
 client.user.setStatus("dnd")
 });
+client.on("message", message => {
+
+            if (message.content.startsWith("$bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'all').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
+});
 
 client.on('message', function(msg) {
 if(msg.content.startsWith ('$Waht')) {
